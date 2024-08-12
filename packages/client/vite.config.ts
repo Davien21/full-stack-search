c872a6b9641d5121fb80972a1e4d2d6e40d6e20c
davien21 <107618227+chidi-locusnine@users.noreply.github.com>
2024-08-12 04:37:55 +0100

    --feat: import magic + vitest setup

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    alias: {
+      "@/utils": path.resolve(__dirname, "utils"),
+      "@/components": path.resolve(__dirname, "src/components"),
+      "@/pages": path.resolve(__dirname, "src/pages"),
+      "@/hooks": path.resolve(__dirname, "src/hooks"),
+    },
+  },
   plugins: [react()],
-})
+});
--- packages/client/vitest.config.ts
+++ packages/client/vitest.config.ts
@@ -1,10 +1,26 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
+  resolve: {
+    alias: {
+      "@/utils": path.resolve(__dirname, "utils"),
+      "@/components": path.resolve(__dirname, "src/components"),
+      "@/pages": path.resolve(__dirname, "src/pages"),
+      "@/hooks": path.resolve(__dirname, "src/hooks"),
+    },
   },
-})
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    include: ["./src/**/*.test.{ts,tsx}"],
+    restoreMocks: true,
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ['src/main.tsx', "src/vite-env.d.ts"],
+      all: true,
+    },
+  },
+});
